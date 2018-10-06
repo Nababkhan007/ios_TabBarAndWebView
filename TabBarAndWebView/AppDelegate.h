bfc1e80188f2554db91ad76fe082349b153a372b
lab <lab@labs-mac-mini-3.local>
2018-10-06 09:39:41 +0600

    Initial Commit

---
+++ TabBarAndWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabBarAndWebView
+//
+//  Created by lab on 10/6/18.
+//  Copyright © 2018 Abir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TabBarAndWebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TabBarAndWebView
+//
+//  Created by lab on 10/6/18.
+//  Copyright © 2018 Abir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
